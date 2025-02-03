63ef227366943ad7bb6fb7507528f436c8a24ae0
Florian Müller <flomueller1981@gmail.com>
2025-02-03 12:58:06 +0100

    update imports

--- library/dist/components/simple-test/simple-test.js
+++ library/dist/components/simple-test/simple-test.js
@@ -1,2 +1,2 @@
-import r from"react";import{cn as i}from"../../utils/style";function p({children:t,className:e,...o}){return r.createElement("div",{className:i("p-2 bg-blue-200 rounded",e),...o},"SimpleTest")}export{p as SimpleTest};
+import*as r from"react";import{cn as i}from"../../utils/style";function n({children:t,className:e,...o}){return r.createElement("div",{className:i("p-2 bg-blue-200 rounded",e),...o},"SimpleTest")}export{n as SimpleTest};
 //# sourceMappingURL=simple-test.js.map
